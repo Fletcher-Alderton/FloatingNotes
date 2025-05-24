e0123cd694a3b9b7dd1a5d9e62bfc88a7d2eecf9
Fletcher Alderton <109054210+Fletcher-Alderton@users.noreply.github.com>
2025-05-24 18:00:54 +1000

    Initial Commit

---
+++ FloatingNotes/FloatingNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloatingNotesApp.swift
+//  FloatingNotes
+//
+//  Created by Fletcher Alderton on 24/5/2025.
+//
+
+import SwiftUI
+
+@main
+struct FloatingNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FloatingNotesTests/FloatingNotesTests.swift
@@ -0,0 +1,17 @@
+//
+//  FloatingNotesTests.swift
+//  FloatingNotesTests
+//
+//  Created by Fletcher Alderton on 24/5/2025.
+//
+
+import Testing
+@testable import FloatingNotes
+
+struct FloatingNotesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
